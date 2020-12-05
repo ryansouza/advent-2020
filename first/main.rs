24de1cb28f7354df338dacf453da112fc5aa90fd
Ryan Souza <rsouza@fastly.com>
2020-12-05 08:35:39 -0800

    Day one part duo

--- first/main.rs
+++ first/main.rs
@@ -4,10 +4,17 @@ fn main() -> io::Result<()> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
-    let entries: Vec<u32> = input.lines().map(|line| line.parse::<u32>().unwrap() ).collect();
+    let entries: Vec<i32> = input.lines().map(|line| line.parse::<i32>().unwrap() ).collect();
 
-    match find_summing_entries(entries) {
+    // I had to switch these to passing borrowed entries
+    // so that I could use it twice. I guess rust gives up ownership by default?
+    match find_summing_entries(&entries) {
         Some((x,y)) => println!("found matching entries: {} * {} = {}", x, y, x * y),
+        None => println!("found no matching entries :("),
+    }
+
+    match find_part2_entries(&entries) {
+        Some((x,y,z)) => println!("found part 2 matching entries: {} * {} * {} = {}", x, y, z, x * y * z),
         None => println!("found no matching entries :("),
     }
 
@@ -15,13 +22,29 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn find_summing_entries(entries: Vec<u32>) -> Option<(u32, u32)> {
-    for (idx, value) in entries.iter().enumerate() {
+fn find_summing_entries(entries: &Vec<i32>) -> Option<(i32, i32)> {
+    for value in entries.iter() {
+        let needed: i32 = 2020 - value;
+
+        if entries.contains(&needed) {
+            return Some((value.clone(), needed));
+        }
+    }
+
+    return None;
+}
+
+
+
+fn find_part2_entries(entries: &Vec<i32>) -> Option<(i32, i32, i32)> {
+    for (idx, value1) in entries.iter().enumerate() {
         let entries_to_check = &entries[idx..];
 
-        for check in entries_to_check.iter() {
-            if check + value == 2020 {
-                return Some((value.clone(), check.clone()));
+        for value2 in entries_to_check.iter() {
+            let needed: i32 = 2020 - value1 - value2;
+
+            if entries.contains(&needed) {
+                return Some((value1.clone(), value2.clone(), needed));
             }
         }
     }
